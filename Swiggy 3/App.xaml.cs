1d8375f8b58f6dc365585b9203137f570e716b1b
Parvez ali <pa7407071463@gmail.com>
2021-06-18 14:47:45 +0530

    Login Page

--- Swiggy 3/App.xaml.cs
+++ Swiggy 3/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Swiggy_3.Pages.ShellPages;
+﻿using Swiggy_3.Pages.Login_Page;
+using Swiggy_3.Pages.ShellPages;
 using Swiggy_3.Pages.SplashScreen;
 using System;
 using Xamarin.Forms;
@@ -12,8 +13,9 @@ namespace Swiggy_3
         {
             InitializeComponent();
 
-            MainPage = new HomePage();
-           // App.Current.MainPage = new NavigationPage(new SplashScreen());
+            MainPage = new NavigationPage(new Loin_Page());
+            //MainPage = new HomePage();
+            //App.Current.MainPage = new NavigationPage(new SplashScreen());
         }
 
         protected override void OnStart()
---
+++ Swiggy 3/Pages/Login Page/Login Page.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Swiggy_3.Pages.Login_Page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Loin_Page : CarouselPage
+    {
+        public Loin_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Verify(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gotoHomePage(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
